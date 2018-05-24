b4c864ef11f72ff10e80ea36ef13687bcbc87ed7
MaryZerep <38222760+MaryZerep@users.noreply.github.com>
2018-05-23 21:06:26 -0500

    2Subida

--- js/acciones3.js
+++ js/acciones3.js
@@ -1,11 +1,23 @@
 // JavaScript Document
-$(document).ready(function(e){
-	document.addEventListener("deviceready", function(){
-		$('#sonar').tap(function(){
-			navigator.notification.beep(2);
-		}); //cierre del beep
-		$('#vibrar').tap(function(){
-			navigator.notification.vibrate(2000);
-		}); //cierre del vibrar
-	},false);//cierre del deviceready
-});//cierre document
+
+$(document).ready(function(e) {
+    document.addEventListener("deviceready",function(){
+		$('#izquierda').on("swipeleft", function(){
+			navigator.notification.alert("deslizó hacia la izquierda",function(){"aplicacion 7","Ok"});
+		});//cerrar deslizar izquierda
+		$('#derecha').on("swiperight",function(){
+			navigator.notification.confirm("¿Que quieres hacer?",function(op){
+				switch(op)
+				{
+					case 1:
+					navigator.notification.beep(1);
+					break;
+					
+					case 2:
+					navigator.notification.vibrate(3000);
+					break;
+				}
+			},"aplicacion 7","Beepear,Vibrar,Cancelar");
+		});//cerrar deslizar derecha
+	},false);//cerrar deviceready
+});//cerrar document
